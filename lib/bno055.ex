73910c0638f8d7582e09d34e8fbcc2f4cd3dd8d7
mmmries <riesmmm@gmail.com>
2019-10-22 08:57:26 -0600

    get started with basic configuration

--- lib/bno055.ex
+++ lib/bno055.ex
@@ -1,18 +1,17 @@
-defmodule Bno055 do
+defmodule BNO055 do
   @moduledoc """
-  Documentation for Bno055.
+  A library for interacting with a BNO055 n-degrees-of-freedom chip
   """
 
-  @doc """
-  Hello world.
+  alias Circuits.I2C
+  alias BNO055.Registers.OprMode
+  @addr 40
 
-  ## Examples
+  defstruct [:i2c, :opts]
 
-      iex> Bno055.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def setup(mode, opts \\ []) do
+    {:ok, ref} = I2C.open(Keyword.get(opts, :i2c_device, "i2c-1"))
+    :ok = I2C.write(ref, @addr, <<OprMode.addr(), OprMode.encode(mode)>>)
+    %__MODULE__{i2c: ref, opts: opts}
   end
 end
---
+++ lib/bno055/registers/opr_mode.ex
@@ -0,0 +1,17 @@
+defmodule BNO055.Registers.OprMode do
+  def addr, do: 0x3D
+
+  def encode(:CONFIGMODE), do: <<0>>
+  def encode(:ACCONLY), do: <<1>>
+  def encode(:MAGONLY), do: <<2>>
+  def encode(:GYROONLY), do: <<3>>
+  def encode(:ACCMAG), do: <<4>>
+  def encode(:ACCGYRO), do: <<5>>
+  def encode(:MAGGYRO), do: <<6>>
+  def encode(:AMG), do: <<7>>
+  def encode(:IMU), do: <<8>>
+  def encode(:COMPASS), do: <<9>>
+  def encode(:M4G), do: <<10>>
+  def encode(:NDOF_FMC_OFF), do: <<11>>
+  def encode(:NDOF), do: <<12>>
+end
